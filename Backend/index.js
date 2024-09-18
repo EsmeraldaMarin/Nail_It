3fcd3d401d6ce02de6ace28bf090cd41d4347349
lucas.romaniello <lucas.romaniello@squad.com.ar>
2024-09-18 11:45:59 -0300

    Primer endpoint de prueba

--- Backend/index.js
+++ Backend/index.js
@@ -1,12 +1,13 @@
 import cors from "cors";
 import express from "express";
 
-
 const PORT = 5050;
-
 const app = express();
-
 app.use(express.json());
 app.use(cors());
+
+app.get("/prueba", (req, res) => {
+    res.send("Funcionando.");
+})
 
 app.listen(PORT, () => console.log(`Servidor corriendo en http://localhost:${PORT}`));
