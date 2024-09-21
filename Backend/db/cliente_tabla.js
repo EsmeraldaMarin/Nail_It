e2c5fe3c6a9b3956dfaab4a3c84a0a3ac7f6f357
lucas.romaniello <lucas.romaniello@squad.com.ar>
2024-09-21 11:00:38 -0300

    Borrando comentarios y funciones de prueba

--- Backend/db/cliente_tabla.js
+++ Backend/db/cliente_tabla.js
@@ -14,27 +14,3 @@ export const Clientes = sequelize.define('Clientes', {
 Clientes.prototype.toString = function (){
     return `|${this.nombre} | ${this.apellido}|${this.email}`
 }
-
-export async function inicializarDatosClientes() {
-    // Sincronizar el modelo con la base de datos
-    await sequelize.sync();
-
-    // Eliminar todos los registros existentes
-    await Clientes.destroy({ where: {}, truncate: true });
-
-    // Crear los nuevos registros
-    await Clientes.bulkCreate([
-        { nombre: 'Alejandro', apellido: 'Castro', email: 'ale.castro@gmail.com', password: 'nail' },
-        { nombre: 'Camila', apellido: 'Mendoza', email: 'cami.men@gmail.com', password: 'nail' },
-        { nombre: 'Fernando', apellido: 'Ruiz', email: 'fer.rui@gmail.com', password: 'nail' },
-        { nombre: 'Isabella', apellido: 'Morales', email: 'isa.mor@gmail.com', password: 'nail' },
-        { nombre: 'Miguel', apellido: 'Vargas', email: 'miguelon@gmail.com', password: 'nail' },
-        { nombre: 'Elena', apellido: 'Jiménez', email: 'elejime@gmail.com', password: 'nail' },
-        { nombre: 'Javier', apellido: 'Torres', email: 'javi.torre@gmail.com', password: 'nail' },
-        { nombre: 'Valeria', apellido: 'Herrera', email: 'vale.herrera@gmail.com', password: 'nail' },
-        { nombre: 'Roberto', apellido: 'Cruz', email: 'rober.cruz@gmail.com', password: 'nail' },
-        { nombre: 'Adriana', apellido: 'Flores', email: 'adri.flower@gmail.com', password: 'nail' },
-    ]);
-};
-
-// inicializarDatosClientes();
