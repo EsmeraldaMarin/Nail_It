3a5c6ce1b162c0173958b4b4feac634a195b3a80
Esmeralda Marín <esmemarinm03@gmail.com>
2024-10-09 10:39:12 -0300

    styles: cambios pequeños en media screen

--- Frontend/src/components/inicio_admin/SideBar.jsx
+++ Frontend/src/components/inicio_admin/SideBar.jsx
@@ -18,6 +18,7 @@ const SideBar = () => {
                             <li className="nav-item"><NavLink end to="/inicio_admin" className={({ isActive }) => (isActive ? 'active' : '')}><i className="bi bi-house"></i>Home</NavLink></li>
                             <li className="nav-item"><NavLink to="/inicio_admin/reservas_pendientes" className={({ isActive }) => (isActive ? 'active' : '')}><i className="bi bi-journal-bookmark"></i>Reservas pendientes</NavLink></li>
                             <li className="nav-item"><NavLink to="/inicio_admin/horarios" className={({ isActive }) => (isActive ? 'active' : '')}><i className="bi bi-calendar-week"></i>Horarios</NavLink></li>
+                            <li className="nav-item"><NavLink to="/inicio_admin/agenda" className={({ isActive }) => (isActive ? 'active' : '')}><i className="bi bi-calendar-week"></i>Agenda</NavLink></li>
                             <li className="nav-item"><NavLink to="/nada" className={({ isActive }) => (isActive ? 'active' : '')}><i className="bi bi-people"></i>Gestion Estilistas</NavLink></li>
                             <li className="nav-item"><NavLink to="/inicio_admin/servicios" className={({ isActive }) => (isActive ? 'active' : '')}><i className="bi bi-bookmarks"></i>Servicios</NavLink></li>
                             <li className="nav-item"><NavLink to="/nada" className={({ isActive }) => (isActive ? 'active' : '')}><i className="bi bi-clock-history"></i>Historial</NavLink></li>
