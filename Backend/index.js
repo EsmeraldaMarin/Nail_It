a5e0c6251154a959521263abc553d9a86aca3fde
lucas.romaniello <lucas.romaniello@squad.com.ar>
2024-10-09 09:38:10 -0300

    ABMC Estado tabla + endpoints

--- Backend/index.js
+++ Backend/index.js
@@ -17,6 +17,8 @@ import { routerReservas } from "./routes/reserva_routes.js";
 import { GestorReservas } from "./controllers/reserva_controller.js";
 import { Reservas } from "./db/reserva_tabla.js";
 import { Clientes } from "./db/cliente_tabla.js";
+import { GestorEstados } from "./controllers/estado_controller.js";
+import { routerEstados } from "./routes/estado_routes.js";
 
 const PORT = 5050;
 
@@ -31,7 +33,8 @@ export const gestorClientes = new GestorClientes();
 export const gestorAdmins = new GestorAdmins();
 export const gestorEspecialidades = new GestorEspecialidades();
 export const gestorServicios = new GestorServicios();
-export const gestorReservas = new GestorReservas()
+export const gestorReservas = new GestorReservas();
+export const gestorEstados = new GestorEstados();
 
 Especialidades.hasMany(Servicios, { foreignKey: 'id_especialidad' });
 Servicios.belongsTo(Especialidades, { foreignKey: 'id_especialidad' });
@@ -51,6 +54,7 @@ app.use("/especialidad", routerEspecialidades);
 app.use("/servicio", routerServicios);
 app.use("/reserva", routerReservas);
 app.use("/verificar", routerVerificar);
+app.use("/estado", routerEstados);
 
 app.listen(PORT, () =>
     console.log(`Servidor corriendo en http://localhost:${PORT}`));
