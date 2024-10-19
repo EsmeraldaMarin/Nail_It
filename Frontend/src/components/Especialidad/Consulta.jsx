843c447e6e19d12674612abcc0154435e528c238
SolMalnis <solmalnis@gmail.com>
2024-10-19 19:33:23 -0300

    creacion de servicios y especialidades

--- Frontend/src/components/Especialidad/Consulta.jsx
+++ Frontend/src/components/Especialidad/Consulta.jsx
@@ -1,35 +1,31 @@
 import "../reserva_admin/Reservas.scss"
 import React, { useState, useEffect } from 'react';
 import axios from '../../axiosConfig/axiosConfig';
+import './Consulta.scss'
 
-export default function ConsultaServicios({servicios , onActualizar}){
-
-    
-    const tbody =  servicios.map((servicio) => (
-            <tr key={servicio.id}>
-                <td>{servicio.nombre}</td>
-                <td>{servicio.precio}</td>
-                <td>{servicio.duracion}</td>
-                <td>{servicio.Especialidad.nombre}</td>
-                <td>
-                 {
-                     (<button className="btn btn-sm btn-secondary"  onClick={()=>{onActualizar(servicio)}}>Actualizar</button>)   
-                 }  
-
+export default function ConsultaServicios({ servicios, onNewClick, onActualizar }) {
+    const tbody = servicios.map((servicio) => (
+        <tr key={servicio.id}>
+            <td>{servicio.nombre}</td>
+            <td>{servicio.precio}</td>
+            <td>{servicio.duracion}</td>
+            <td>{servicio.Especialidad.nombre}</td>
+            <td>
+                <button className="btn btn-sm btn-secondary" onClick={() => { onActualizar(servicio) }}>
+                    Actualizar
+                </button>
             </td>
-
-                
-            </tr>
-        ));
-
-
-    
-
-
+        </tr>
+    ));
 
     return (
         <div className='container-fluid Reservas'>
-            <h3>Gestor de Servicios</h3>
+            <div className="d-flex justify-content-between align-items-center">
+                <h3>Gestor de Servicios</h3>
+                <button type="button" className="active" onClick={onNewClick}>
+                    Registrar servicio
+                </button>
+            </div>
 
             {/* Reservas del estilista */}
             <div className="table-ctn mis-reservas" style={{ overflowX: "auto" }}>
@@ -49,7 +45,6 @@ export default function ConsultaServicios({servicios , onActualizar}){
             </div>
         </div>
     );
-
-};
+}
 
 
--- Frontend/src/components/Especialidad/RegistrarServicio.jsx
+++ Frontend/src/components/Especialidad/RegistrarServicio.jsx
@@ -3,34 +3,34 @@ import React, { useState, useEffect } from 'react';
 import {useForm} from 'react-hook-form' 
 import axios from '../../axiosConfig/axiosConfig';
 
-export default function RegistrarServicio({onCancelar, item, actualizado}){
+
+
+export default function RegistrarServicio({onGuardar, onCancelar, item, actualizado,  registrarEsp, especialidades}){
     const {register, handleSubmit, formState: {errors}, setValue} = useForm({values: item})
 
-    const [especialidades, setEspecialidades] = useState([]);
+    
 
     useEffect(() => {
-        const fetchEspecialidades = async () => {
-            try {
-                const response = await axios.get('/especialidad');
-                setEspecialidades(response.data);
+        
                 if (item && item.id_especialidad) {
                     setValue('id_especialidad', item.id_especialidad);
                 }
-                
-            } catch (error) {
-                console.error('Error al obtener las especialidades', error);
-            }
-        };
-        fetchEspecialidades();
-    }, []);
+            } );
+
 
     const onSubmit = (data) => {
         if (item.id)
             actualizado(item.id,data) 
         else
-            console.log('aca guardo')
+            onGuardar(data)
     }
+    const especialidades2 = especialidades
 
+
+
+
+
+    
     return(
         <>
             <form onSubmit={handleSubmit(onSubmit)}>
@@ -48,8 +48,8 @@ export default function RegistrarServicio({onCancelar, item, actualizado}){
                     {errors.precio && <span className="text-danger">{errors.precio.message}</span>}
                 </div>
                 <div className="form-group">
-                    <label htmlFor="duracion">Duracion del servicio (en minutos)</label>
-                    <textarea type="number" className="form-control" id="duracion" placeholder="Ingrese el precio (sin $)"
+                    <label htmlFor="duracion">Duracion del servicio (en minutos) </label>
+                    <textarea type="number" className="form-control" id="duracion" placeholder="Ingrese la duracion (en min)"
                     {...register('duracion', {required: 'Campo obligatorio'})} />
                     {errors.duracion && <span className="text-danger">{errors.duracion.message}</span>}
                 </div>
@@ -62,7 +62,7 @@ export default function RegistrarServicio({onCancelar, item, actualizado}){
                         {...register('id_especialidad', { required: 'Debe seleccionar una especialidad' })}
                     >
                         <option value="">Seleccione una especialidad</option>
-                        {especialidades.map((especialidad) => (
+                        {especialidades2.map((especialidad) => (
                             <option key={especialidad.id} value={especialidad.id}>
                                 {especialidad.nombre}
                             </option>
@@ -70,6 +70,13 @@ export default function RegistrarServicio({onCancelar, item, actualizado}){
                     </select>
                     {errors.especialidad && <span className="text-danger">{errors.especialidad.message}</span>}
                 </div>
+                {item.id === undefined && (
+                    <div className="form-group mt-2">
+                        <button type="button" className="btn btn-link" onClick={registrarEsp}>
+                            Agregar nueva especialidad
+                        </button>
+                    </div>
+                )}
                 <div className="form-group mt-3">
                     <button type="submit" className="btn btn-primary mx-1">Guardar</button>
                     <button type="button" onClick={onCancelar}  className="btn btn-secondary mx-1"> Cancelar</button>
