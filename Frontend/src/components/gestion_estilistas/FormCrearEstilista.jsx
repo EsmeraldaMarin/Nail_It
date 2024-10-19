f49d6a78ccc5f0781fbea80e793cc256e5df85c6
Esmeralda Marín <esmemarinm03@gmail.com>
2024-10-19 14:25:09 -0300

    fix: cierre de modal y offcanvas

--- Frontend/src/components/gestion_estilistas/FormCrearEstilista.jsx
+++ Frontend/src/components/gestion_estilistas/FormCrearEstilista.jsx
@@ -1,13 +1,22 @@
+import React from 'react';
 import Registro from "../login/Registro";
 
 function FormCrearEstilista({ modalRef, profesionales, setProfesionales, setFilteredProfesionales }) {
     const handleSubmitAdmin = (newProfesional) => {
+        // Actualizar las listas de profesionales
         setProfesionales([...profesionales, newProfesional]);
         setFilteredProfesionales([...profesionales, newProfesional]);
+
+        // Cerrar el modal al terminar
         const modalElement = modalRef.current;
-        const bootstrapModal = new window.bootstrap.Modal(modalElement);
-        bootstrapModal.hide();
+        if (modalElement) {
+            const bootstrapModal = window.bootstrap.Modal.getInstance(modalElement); // Obtener la instancia existente
+            if (bootstrapModal) {
+                bootstrapModal.hide(); // Cerrar el modal
+            }
+        }
     }
+
     return (
         <div className="modal fade" id="FormCrearEstilista" tabIndex="-1" aria-labelledby="modalLabel" aria-hidden="true" ref={modalRef}>
             <div className="modal-dialog modal-dialog-scrollable">
@@ -17,7 +26,13 @@ function FormCrearEstilista({ modalRef, profesionales, setProfesionales, setFilt
                         <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div className="modal-body-crear-estilista">
-                        <Registro mensajeBoton={"Crear Estilista"} isAdminParam={true} redirect="/inicio_admin/gestion_estilistas" handleSubmitAdmin={handleSubmitAdmin} />
+                        {/* Formulario para crear una nueva estilista */}
+                        <Registro
+                            mensajeBoton={"Crear Estilista"}
+                            isAdminParam={true}
+                            redirect="/inicio_admin/gestion_estilistas"
+                            handleSubmitAdmin={handleSubmitAdmin}
+                        />
                     </div>
                 </div>
             </div>
