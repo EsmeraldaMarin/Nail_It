2523c063ab98d763cdb16a4e13f6be39f547b510
Esmeralda Marín <esmemarinm03@gmail.com>
2024-10-22 00:26:31 -0300

    fix: correccion en error de fecha. urgente hacer pull

--- Frontend/src/components/reserva_admin/ReservasConfirmadas.jsx
+++ Frontend/src/components/reserva_admin/ReservasConfirmadas.jsx
@@ -3,19 +3,14 @@ import { format, isSameDay } from 'date-fns';
 import { es } from 'date-fns/locale';
 import React, { useState, useEffect } from 'react';
 import axios from '../../axiosConfig/axiosConfig';
-
 const ReservasConfirmadas = () => {
     const [reservas, setReservas] = useState([]);
     const userId = localStorage.getItem('userId');
 
-    // Función para formatear la fecha
     const formatearFecha = (fecha) => {
         const fechaLocal = new Date(new Date(fecha).getTime() + new Date().getTimezoneOffset() * 60000);
         return format(fechaLocal, 'EEEE dd/MM', { locale: es });
     };
-
-    // Obtener fecha de hoy
-    const hoy = new Date();
 
     useEffect(() => {
         const fetchReservas = async () => {
@@ -29,25 +24,28 @@ const ReservasConfirmadas = () => {
         fetchReservas();
     }, []);
 
-    // Filtrar reservas para hoy y del estilista
+    // Obtener la fecha de hoy
+    const hoy = new Date();
+
+    // Filtrar reservas por estilista y fecha de hoy
     const reservasEstilista = reservas.filter(
-        reserva => reserva.estado === "confirmada" &&
+        reserva =>
+            reserva.estado === "confirmada" &&
             reserva.id_profesional === userId &&
-            isSameDay(new Date(reserva.fecha), hoy) // Filtrar por fecha de hoy
+            isSameDay(new Date(new Date(new Date(reserva.fecha).getTime() + new Date().getTimezoneOffset() * 60000)), hoy) // Comparar la fecha con el día de hoy
     );
 
-    // Filtrar todas las reservas confirmadas para hoy
+    // Filtrar reservas generales confirmadas y fecha de hoy
     const reservasConfirmadas = reservas.filter(
-        reserva => reserva.estado === "confirmada" &&
-            isSameDay(new Date(reserva.fecha), hoy) // Filtrar por fecha de hoy
+        reserva =>
+            reserva.estado === "confirmada" &&
+            isSameDay(new Date(new Date(new Date(reserva.fecha).getTime() + new Date().getTimezoneOffset() * 60000)), hoy) // Comparar la fecha con el día de hoy
     );
-
-    // Función para renderizar filas de la tabla
     const renderFilasReserva = (reservasFiltradas) => {
         return reservasFiltradas.map((reserva, index) => (
             <tr key={index}>
-                <td className="text-capitalize text-wrap" style={{ width: "10rem" }}>{reserva.Servicio.nombre}</td>
-                <td className="text-capitalize text-wrap" style={{ width: "8rem" }}>{reserva.Cliente.nombre} {reserva.Cliente.apellido}</td>
+                <td className="text-capitalize">{reserva.Servicio.nombre}</td>
+                <td className="text-capitalize">{reserva.Cliente.nombre}</td>
                 <td>{reserva.Cliente.numero}</td>
                 <td className="text-capitalize">{formatearFecha(reserva.fecha)}</td>
                 <td>{reserva.horaInicio}</td>
@@ -62,71 +60,68 @@ const ReservasConfirmadas = () => {
             </tr>
         ));
     };
-
     return (
         <div className='container-fluid Reservas'>
             <h3>Gestor de turnos</h3>
 
             {/* Reservas del estilista */}
             <h4 className="py-3">Tus turnos de hoy</h4>
-            <div className="table-ctn mis-reservas pb-2">
-                <div className="table-responsive">
-                    <table className="table">
-                        <thead className="table-primary">
-                            <tr>
-                                <th scope="col">Servicio</th>
-                                <th scope="col">Cliente</th>
-                                <th scope="col">Teléfono</th>
-                                <th scope="col">Fecha Turno</th>
-                                <th scope="col">Hora Turno</th>
-                                <th scope="col">Precio servicio</th>
-                                <th scope="col">Importe abonado</th>
-                                <th scope="col">Importe a cobrar</th>
-                                <th scope="col">Acciones</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {reservasEstilista.length > 0
-                                ? renderFilasReserva(reservasEstilista)
-                                : <tr><td colSpan="9">No tienes reservas confirmadas para hoy</td></tr>}
-                        </tbody>
-                    </table>
-                </div>
+            <div className="table-ctn mis-reservas table-responsive">
+                <table className="table ">
+                    <thead className="table-primary">
+                        <tr>
+                            <th scope="col">Servicio</th>
+                            <th scope="col">Cliente</th>
+                            <th scope="col">Teléfono</th>
+                            <th scope="col">Fecha Turno</th>
+                            <th scope="col">Hora Turno</th>
+                            <th scope="col">Precio servicio</th>
+                            <th scope="col">Importe abonado</th>
+                            <th scope="col">Importe a cobrar</th>
+                            <th scope="col">Acciones</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {reservasEstilista.length > 0
+                            ? renderFilasReserva(reservasEstilista)
+                            : <tr><td colSpan="5">No tienes reservas confirmadas para hoy</td></tr>}
+                    </tbody>
+                </table>
             </div>
 
             {/* Reservas generales */}
             <h4 className="py-3">Turnos generales de hoy</h4>
-            <div className="table-ctn reservas-gral pb-2">
-                <div className="table-responsive">
-                    <table className="table">
-                        <thead className="table-dark">
-                            <tr>
-                                <th scope="col">Estilista</th>
-                                <th scope="col">Servicio</th>
-                                <th scope="col">Cliente</th>
-                                <th scope="col">Teléfono</th>
-                                <th scope="col">Fecha Turno</th>
-                                <th scope="col">Hora Turno</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {reservasConfirmadas.length > 0
-                                ? reservasConfirmadas.map((reserva, index) => (
-                                    <tr key={index}>
-                                        <th className="text-uppercase">{reserva.Admin.nombre}</th>
-                                        <td>{reserva.Servicio.nombre}</td>
-                                        <td className="text-capitalize ">{reserva.Cliente.nombre} {reserva.Cliente.apellido}</td>
-                                        <td>{reserva.Cliente.numero}</td>
-                                        <td className="text-capitalize">{formatearFecha(reserva.fecha)}</td>
-                                        <td>{reserva.horaInicio}</td>
-                                    </tr>
-                                ))
-                                : <tr><td colSpan="6">No hay reservas confirmadas para hoy</td></tr>}
-                        </tbody>
-                    </table></div>
+            <div className="table-ctn reservas-gral table-responsive">
+                <table className="table">
+                    <thead className="table-dark">
+                        <tr>
+                            <th scope="col">Estilista</th>
+                            <th scope="col">Servicio</th>
+                            <th scope="col">Cliente</th>
+                            <th scope="col">Teléfono</th>
+                            <th scope="col">Fecha Turno</th>
+                            <th scope="col">Hora Turno</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {reservasConfirmadas.length > 0
+                            ? reservasConfirmadas.map((reserva, index) => (
+                                <tr key={index}>
+                                    <th className="text-uppercase">{reserva.Admin.nombre}</th>
+                                    <td>{reserva.Servicio.nombre}</td>
+                                    <td className="text-capitalize">{reserva.Cliente.nombre}</td>
+                                    <td>{reserva.Cliente.numero}</td>
+                                    <td className="text-capitalize">{formatearFecha(reserva.fecha)}</td>
+                                    <td>{reserva.horaInicio}</td>
+                                </tr>
+                            ))
+                            : <tr><td colSpan="6">No hay reservas confirmadas para hoy</td></tr>}
+                    </tbody>
+                </table>
             </div>
         </div>
     );
 };
+
 
 export default ReservasConfirmadas;
